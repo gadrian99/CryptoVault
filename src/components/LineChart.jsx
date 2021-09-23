e80f452050cfe2d3c9dcc80adf2b1494a03e4664
gadrian99 <adriangarcia1999@yahoo.com>
2021-09-22 21:09:02 -0400

    line chart and historic data visual

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -6,7 +6,39 @@ import { Col, Row, Typography } from 'antd'
 const { Title } = Typography
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
-    // console.log(coinHistory)
+    const coinPrice = []
+    const coinTimeStamp = []
+
+    for(let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+        coinPrice.push(coinHistory.data.history[i].price)
+        coinTimeStamp.push(new Date(coinHistory.data.history[i].timestamp).toLocaleDateString())
+    }
+
+    const data = {
+        labels: coinTimeStamp,
+        datasets: [
+            {
+                label: 'Price in USD',
+                data: coinPrice,
+                fill: false,
+                backgroundColor: '#0071bd',
+                borderColor: '#0071bd'
+            }
+        ]
+    }
+
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            ]
+        }
+    }
+
     return (
         <>
             <Row className="chart-header">
@@ -15,6 +47,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
                     <Title level={5} className="price-change">{coinHistory?.data?.change}%</Title>
                     <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
                 </Col>
+                <Line data={data} options={options} />
             </Row>
         </>
     )
