dabfa26514aabdee72243b662dd5770993c0d6f7
gadrian99 <adriangarcia1999@yahoo.com>
2021-09-22 21:30:10 -0400

    loader component and implementation into rest of app

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,6 +4,7 @@ import moment from 'moment'
 
 import { useGetCryptosQuery } from '../services/cryptoApi'
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+import Loader from './Loader'
 
 const { Text, Title } = Typography
 const { Option } = Select
@@ -16,7 +17,7 @@ const News = ({ simplified }) => {
     const { data } = useGetCryptosQuery(100)
 
 
-    if(!cryptoNews?.value) return 'Loading...'
+    if(!cryptoNews?.value) return <Loader />
 
     return (
         <div>
