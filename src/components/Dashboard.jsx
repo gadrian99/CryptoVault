60f1838b3d170e93a330c8ec69c213b99cc81ea7
Adrian Garciia <adriangarcia1999@yahoo.com>
2021-09-30 03:48:54 -0400

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,84 @@
-import React from 'react'
-import { Typography } from 'antd'
+import React, { useState, useEffect }from 'react'
+import { Typography, Button, Table } from 'antd'
 import { useMoralis } from "react-moralis";
+
+import Loader from './Loader'
 
 const { Title } = Typography
 
 const Dashboard = () => {
-    const { authenticate, isAuthenticated, user } = useMoralis();
+    const [txs, setTxs] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+    const { Moralis, user } = useMoralis()
+    // console.log(txs)
+    useEffect(async () => {
+        setLoading(true)
+        try {
+            await Moralis.Web3API.account.getTransactions()
+                .then((data) => {setTxs(data.result)})
+        } catch (err) {
+            setError(true)
+        }
+        setLoading(false)
+    }, [])
+    
+    // Table Data
+    const columns = [
+        {
+            title: 'Date',
+            dataIndex: 'date',
+            key: 'date',
+        },
+        {
+            title: 'Hash',
+            dataIndex: 'hash',
+            key: 'hash',
+        },
+        {
+            title: 'From',
+            dataIndex: 'from',
+            key: 'from',
+        },
+        {
+            title: 'To',
+            dataIndex: 'to',
+            key: 'to',
+        },
+    ]
 
-    if (!isAuthenticated) {
-        return (
-            <div>
-                <button onClick={() => authenticate()}>Authenticate</button>
-            </div>
-        );
+    const generateData = () => {
+        let sourceData = []
+        
+        txs.map((tx,  i = 0) => {
+            sourceData.push(
+                {
+                    date: tx.block_timestamp,
+                    hash: tx.hash,
+                    from: tx.from_address,
+                    to: tx.to_address,
+                }
+            )
+        })
+        return sourceData
     }
+    const dataSource = generateData()
 
-  return (
+
+    if (loading) { return( <Loader /> )}
+    if (error) { return( <div>Error</div>)}
+    return(
         <div>
-            <h1>Welcome {user.get("username")}</h1>
+            <Button onClick={() => console.log(dataSource)}>Get</Button>
+            <Title level={2}>Dashboard</Title>
+            <Table dataSource={dataSource} columns={columns} />
+            {/* {txs.length != 0 && (
+                txs.map((tx) => (
+                    <p>{tx.hash}</p>
+                ))
+            )} */}
         </div>
-  );
+    )
 
 }
 
